1f9d7150b174c0b5c5e2beb2972f0ea2b6125657
keyursolanki7071 <keyur.solanki@arsenaltech.com>
2025-04-01 23:17:02 +0530

    Installed ShadCN

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import { Button } from "@/components/ui/button"
+
 function App() {
 
   return (
-    <>
-      <h1>My APP</h1>
-    </>
+    <div className="flex flex-col items-center justify-center min-h-svh">
+      <Button>Click me</Button>
+    </div>
   )
 }
 
